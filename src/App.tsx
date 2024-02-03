b19f195716cc6e3612b47599b4874bbaa8020973
nissano <nissano@wix.com>
2024-02-03 13:33:08 +0200

    mapping elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import { Logo } from './Logo';
 import { Counter } from './Counter';
 import { useState } from 'react';
+import { logos } from './logos';
 
 // App Component
 function App() {
@@ -18,18 +17,18 @@ function App() {
   return (
     <>
       <div>
-        <Logo
-          href="https://vitejs.dev"
-          src={viteLogo}
-          alt="Vite logo"
-          className="logo"
-        />
-        <Logo
-          href="https://react.dev"
-          src={reactLogo}
-          alt="React logo"
-          className="logo react"
-        />
+        {logos.map((logo, idx) => (
+          // Loop through the logos array and render the Logo component
+          <Logo
+            key={idx} // Key is required for list items in React to help with rendering performance and identity of elements.
+            // Pass the props to the Logo component:
+            src={logo.src}
+            href={logo.href}
+            alt={logo.alt}
+            className={logo.className}
+          />
+          // We could suger it like this: <Logo key={logo.alt} {...logo} />
+        ))}
       </div>
       <h1>Vite + React</h1>
       <h2>Counter: {count}</h2>
---
+++ src/logos.ts
@@ -0,0 +1,42 @@
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+
+interface Logo {
+  href: string;
+  src: string;
+  alt: string;
+  className: string;
+}
+
+export const logos: Logo[] = [
+  {
+    href: 'https://vitejs.dev',
+    src: viteLogo,
+    alt: 'Vite logo',
+    className: 'logo',
+  },
+  {
+    href: 'https://react.dev',
+    src: reactLogo,
+    alt: 'React logo',
+    className: 'logo react',
+  },
+  {
+    href: 'https://react.dev',
+    src: reactLogo,
+    alt: 'React logo',
+    className: 'logo react',
+  },
+  {
+    href: 'https://react.dev',
+    src: reactLogo,
+    alt: 'React logo',
+    className: 'logo react',
+  },
+  {
+    href: 'https://react.dev',
+    src: reactLogo,
+    alt: 'React logo',
+    className: 'logo react',
+  },
+];
